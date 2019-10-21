2e8471f71dc2bec13c5b8f617511247969d8edd9
resplandeluiz <luizresplandeoliveira@gmail.com>
2019-10-21 20:14:43 -0300

    :rocket: :sparkles: aula 11 finalizada

--- Aula11/app/src/main/java/com/example/a17114290048/aula11/MainActivity.kt
+++ Aula11/app/src/main/java/com/example/a17114290048/aula11/MainActivity.kt
@@ -1,11 +1,39 @@
+package com.example.a17114290048.aula11
+
 import android.app.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
+
+     override fun onCreate(savedInstanceState: Bundle?) {
+         println("onCreate disparado")
+         setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
     }
 
+    override fun onStart(){
+        println("onStart disparado")
+        super.onStart()
+    }
+
+    override fun onResume(){
+        println("onResume disparado")
+        super.onResume()
+    }
+
+    override fun onPause(){
+        println("onPause disparado")
+        super.onPause()
+    }
+
+    override fun onStop(){
+        println("onStop disparado")
+        super.onStop()
+    }
+    override fun onDestroy(){
+        println("onDestroy disparado")
+        super.onDestroy()
+    }
 }
