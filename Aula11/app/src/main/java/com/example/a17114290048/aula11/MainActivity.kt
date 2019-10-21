60755878897839a34530a30fd6731ffd16f54c1f
resplandeluiz <luizresplandeoliveira@gmail.com>
2019-10-21 19:57:21 -0300

    :rocket: iniciando aula11

---
+++ Aula11/app/src/main/java/com/example/a17114290048/aula11/MainActivity.kt
@@ -0,0 +1,11 @@
+import android.app.Activity
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
