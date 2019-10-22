1be63d9ff642a912cb1ef3423eac18b4c5fbcbd2
resplandeluiz <luizresplandeoliveira@gmail.com>
2019-10-21 21:00:14 -0300

    :rocket: :sparkles: atividade12 finalizada

--- Aula12/app/src/main/java/com/example/a17114290048/aula12/ChildActivity.kt
+++ Aula12/app/src/main/java/com/example/a17114290048/aula12/ChildActivity.kt
@@ -25,33 +25,35 @@ class ChildActivity : AppCompatActivity() {
 
         val botao = findViewById<View>(R.id.button) as Button
         botao.setOnClickListener(object : View.OnClickListener {
-           override fun onClick(v: View) {
-                val intencao = Intent(this@MainActivity, ChildActivity::class.java)
+            override fun onClick(v: View) {
+                val intencao = Intent(this@ChildActivity, MainActivity::class.java)
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                val pendente = PendingIntent.getActivity(this@MainActivity, 0, intencao, 0)
+                val pendente = PendingIntent.getActivity(this@ChildActivity, 0, intencao, 0)
+
+                val CHANNEL_ID = "meu_canal_id"
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    val name = "meu_canal"
+                    val description = "teste de notificacao"
+                    val importance = NotificationManager.IMPORTANCE_DEFAULT
+                    val channel = NotificationChannel(CHANNEL_ID, name, importance)
+                    channel.description = description
+                    val notificationManager = getSystemService(NotificationManager::class.java)
+                    notificationManager!!.createNotificationChannel(channel)
+                }
+
+                val builder = NotificationCompat.Builder(this@ChildActivity, CHANNEL_ID)
+                builder.setSmallIcon(android.R.drawable.ic_dialog_info)
+                    .setContentTitle("Minha notificacao")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .setContentIntent(pendente)
+                    .setContentText("Chegou uma notificacao importante!")
+
+                val manager = NotificationManagerCompat.from(this@ChildActivity)
+                manager.notify(123, builder.build())
+
             }
         })
 
-        val CHANNEL_ID = "meu_canal_id"
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "meu_canal"
-            val description = "teste de notificacao"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance)
-            channel.description = description
-            val notificationManager = getSystemService(NotificationManager::class.java)
-            notificationManager!!.createNotificationChannel(channel)
-        }
-
-        val builder = NotificationCompat.Builder(this@MinhaActivity, CHANNEL_ID)
-        builder.setSmallIcon(android.R.drawable.ic_dialog_info)
-            .setContentTitle("Minha notificacao")
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentIntent(pendente)
-            .setContentText("Chegou uma notificacao importante!")
-
-        val manager = NotificationManagerCompat.from(this@MinhaActivity)
-        manager.notify(123, builder.build())
 
     }
 }
