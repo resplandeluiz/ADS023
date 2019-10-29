62d96a803accabdaf59d18bb1ed3412e50d1e979
resplandeluiz <luizresplandeoliveira@gmail.com>
2019-10-28 21:47:46 -0300

    :bug: :construction: fazendo atividade15

---
+++ Aula15/app/src/main/java/aula15/a17114290048/aula15/NovoRequerimentoActivity.kt
@@ -0,0 +1,12 @@
+package aula15.a17114290048.aula15
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NovoRequerimentoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo_requerimento)
+    }
+}
