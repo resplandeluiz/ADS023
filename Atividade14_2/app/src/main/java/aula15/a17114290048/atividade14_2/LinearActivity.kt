9f407f70fe60f3f1d1d8d647b774dfed81e36745
resplandeluiz <luizresplandeoliveira@gmail.com>
2019-10-28 21:23:04 -0300

    :sparkles: :rocket: Atividade 14.2 finalizada

---
+++ Atividade14_2/app/src/main/java/aula15/a17114290048/atividade14_2/LinearActivity.kt
@@ -0,0 +1,12 @@
+package aula15.a17114290048.atividade14_2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear)
+    }
+}
