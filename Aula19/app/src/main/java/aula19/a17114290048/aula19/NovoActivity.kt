63c8d8c6768573c85062404a0da89b09cc7dfd43
resplandeluiz <luizresplandeoliveira@gmail.com>
2019-11-22 22:18:58 -0200

    :rocket: :sparkles: Aula19 iniciando

---
+++ Aula19/app/src/main/java/aula19/a17114290048/aula19/NovoActivity.kt
@@ -0,0 +1,12 @@
+package aula19.a17114290048.aula19
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NovoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_novo)
+    }
+}
